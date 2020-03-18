682d1b9cf69968c3c582d0f8865362911f80fafd
DenisioMytnysiano <den.mytnyk@ua.fm>
2020-03-18 11:44:41 +0200

    Node and BinaryHeap class implemented

---
+++ AStar/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    class Node
+    {
+        public int x;
+        public int y;
+        public int f;
+        public int h;
+        public int g;
+        public Node parent;
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class BinaryHeap
+    {
+        public List<Node> items = new List<Node> { };
+
+        public int parent(int index) { return (index - 1) / 2; }
+        public void SiftUp(int i)
+        {
+            while (i != 0 && items[parent(i)].f > items[i].f)
+            {
+                Node tmp = items[i];
+                items[i] = items[parent(i)];
+                items[parent(i)] = tmp;
+            }
+        }
+        public void SiftDown(int i)
+        {
+            while (2 * i + 1 < items.Count)
+            {
+                int j = i;
+                if (items[2 * i + 1].f < items[i].f)
+                {
+                    j = 2 * i + 1;
+                }
+                if (2 * i + 2 < items.Count && items[2 * i + 2].f < items[j].f)
+                {
+                    j = 2 * i + 2;
+                }
+                if (i == j) break;
+                Node tmp = items[i];
+                items[i] = items[j];
+                items[j] = tmp;
+            }
+        }
+        public void Insert(Node k)
+        {
+
+            items.Add(k);
+            this.SiftUp(items.Count - 1);
+
+        }
+        public Node ExtractMin()
+        {
+            if (items.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Node tmp = items[0];
+                items[0] = items[items.Count - 1];
+                items.RemoveAt(0);
+                this.SiftDown(0);
+                return tmp;
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                return items.Count == 0;
+            }
+        }
+        public static void Heapify(List<Node> arr, BinaryHeap heap)
+        {
+            foreach (var elem in arr)
+            {
+                heap.Insert(elem);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
